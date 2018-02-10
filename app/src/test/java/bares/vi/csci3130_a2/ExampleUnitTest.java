5b053a78cea81e949fd47b26223d1c34110522f1
vibar <victor.bares@ensta-bretagne.org>
2018-02-10 01:28:19 -0400

    The three other cases

--- app/src/test/java/bares/vi/csci3130_a2/ExampleUnitTest.java
+++ app/src/test/java/bares/vi/csci3130_a2/ExampleUnitTest.java
@@ -19,13 +19,22 @@ public class ExampleUnitTest {
         Validator valid = new Validator();
 
         // Test password word
-        assertTrue((valid.validate("passWorD")>1));
+        assertTrue((valid.validate("passWorD")>=1));
 
         //test not long enough
-        assertTrue((valid.validate("Notlong")>1));
+        assertTrue((valid.validate("Notlong")>=1));
+
+        //test require digit
+        assertTrue((valid.validate("Nodigita")>=1));
+
+        //test upper and lower case
+        assertTrue((valid.validate("nolowupp1")>=1));
+
+        //test special character
+        assertTrue((valid.validate("NospecialC1")>=1));
 
         //test password working
-        assertTrue((valid.validate("123456789")==1));
+        assertTrue((valid.validate("123456789")==0));
 
 
     }
